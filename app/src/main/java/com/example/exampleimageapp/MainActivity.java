20599a05298d10f671fcfab12b3e5478953e2528
vatsks <iamksvats@gmail.com>
2021-06-21 22:55:04 +0530

    Added a loop for functionality.

--- app/src/main/java/com/example/exampleimageapp/MainActivity.java
+++ app/src/main/java/com/example/exampleimageapp/MainActivity.java
@@ -8,11 +8,18 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    public int k=0;
 
     public void click(View view){
 
         ImageView image = (ImageView) findViewById(R.id.imageViewSmile);
+        if (k%2==0){
         image.setImageResource(R.drawable.sedface);
+        }
+        else {
+            image.setImageResource(R.drawable.smilingface);
+        }
+        k++;
 
         Log.i("info","log success");
 
